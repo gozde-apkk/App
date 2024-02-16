bcdc196432ebca8aa8aee73f2b40aa279b73e8eb
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-16 14:19:00 +0300

    producer model update

--- App.Web/Models/Producer.cs
+++ App.Web/Models/Producer.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using App.Web.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Web.Models
 {
-    public class Producer
+    public class Producer :IEntityBase
     {
         [Key]
         public int Id { get; set; }
