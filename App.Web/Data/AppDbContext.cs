347c777cffa3c641dab636e0aba1ad6d26e59695
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-16 14:19:57 +0300

    created Appdbcontext

---
+++ App.Web/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using App.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Web.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.MovieId,
+                am.ActorId
+            });
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.ActorId);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Actor_Movie> Actor_Movies { get; set; }
+        public DbSet<Cinema> Cinema { get; set; }
+        public DbSet<Producer> Producer { get; set; }
+
+    }
+}
