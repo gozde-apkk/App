a5077230e47efd4353d3cfceaecb8309b2d342bc
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-18 10:19:11 +0300

    created cinema controller

---
+++ App.Web/Controllers/CinemaController.cs
@@ -0,0 +1,23 @@
+﻿using App.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Web.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemaController(AppDbContext context)
+        {
+            _context = context;
+        }
+      
+
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinema.ToListAsync();
+            return View(allCinemas);
+        }
+    }
+}
