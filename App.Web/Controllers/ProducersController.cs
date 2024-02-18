48fff14830c071e6777e5808ce341af89a7b1c2c
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-18 10:21:16 +0300

    created producer controller

---
+++ App.Web/Controllers/ProducersController.cs
@@ -0,0 +1,36 @@
+﻿using App.Web.Data;
+using App.Web.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Web.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly IProducerService _service;
+
+        public ProducersController(IProducerService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if(producerDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(producerDetails);
+        }
+        public IActionResult Create() 
+        {
+            return View();
+        }
+    }
+}
