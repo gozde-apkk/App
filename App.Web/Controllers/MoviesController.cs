b7d0113c046bf0bf2de1b21c50cdadb00694761b
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-18 10:20:01 +0300

    created movie controller

---
+++ App.Web/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using App.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var allMovies = await _context.Movie.Include(m => m.Cinema).OrderBy(m => m.Name).ToListAsync();
+            return View(allMovies);    
+        }
+    }
+}
