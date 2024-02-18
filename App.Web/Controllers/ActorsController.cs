10d57809dabc51cc9467429dc3bb0674b815fd5b
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-18 10:18:21 +0300

    created actor controller

---
+++ App.Web/Controllers/ActorsController.cs
@@ -0,0 +1,80 @@
+﻿using App.Web.Data;
+using App.Web.Data.Services;
+using App.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IActorService _service;
+
+        public ActorsController(IActorService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _service.GetAllAsync();   
+            return View(data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName, ProfilePictureURL,Bio")]Actor actor)
+        {
+          
+            _service.AddAsync(actor);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null) return View("Empty"); 
+
+             return View(actorDetails);
+
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,[Bind("Id,FullName, ProfilePictureURL,Bio")] Actor actor)
+        {
+
+            await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        //Get: Actors/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
