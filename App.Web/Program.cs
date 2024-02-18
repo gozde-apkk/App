4fe75eb88ebb29c6a4c2447931ec737a154368bd
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-18 10:21:54 +0300

    launchSettings.json

---
+++ App.Web/Program.cs
@@ -0,0 +1,39 @@
+using App.Web.Data;
+using App.Web.Data.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));  
+
+
+
+//Services configuration    
+builder.Services.AddScoped<IActorService , ActorService>();
+builder.Services.AddScoped<IProducerService, ProducersService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed Database
+AppDbInitializer.Seed(app);
+app.Run();
